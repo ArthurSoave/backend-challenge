5edc0402d3f00d523ecb3d771e1faf6b9a486c18
Arthur Soave <atrpaula@gmail.com>
2021-10-14 20:33:36 -0300

    WIP - implementacao da validacao do atributo senha a partir de pattern regex

---
+++ src/main/java/com/soave/backendchallenge/domain/model/Senha.java
@@ -0,0 +1,40 @@
+package com.soave.backendchallenge.domain.model;
+
+import java.util.regex.Pattern;
+
+/**
+ * Classe responsavel por lidar e manipular a senha a ser validada
+ *
+ * @author arthursoave
+ * @since 10/2021
+ */
+public class Senha {
+
+    /**
+     * Padrao de Regex que garante que a String senha cumpra com os requisitos necessarios para que seja considerada
+     * valida:
+     * ^ ≥ inicio da String
+     * (?=.*[A-Z]) ≥ ao menos uma letra maiúscula
+     * (?=.*[!@#$%^&*()-+]) ≥  ao menos um caractere especial
+     * (?=.*[a-z]) ≥ ao menos uma letra minuscula
+     * (?=.*[0-9]) ≥ ao menos um digito numerico
+     * .{9,}$ ≥ no minimo 9 caracteres
+     *
+     */
+    private static final String PATTERN = "^(?=.*[A-Z])(?=.*[!@#$%^&*()-+])(?=.*[a-z])(?=.*[0-9]).{9,}$";
+
+    private boolean isValid;
+
+    public Senha(String senha){
+        this.isValid = validaSenha(senha);
+    }
+
+    public boolean getIsValid(){
+        return this.isValid;
+    }
+
+    private boolean validaSenha(String senha){
+        return Pattern.matches(PATTERN, senha);
+    }
+
+}
